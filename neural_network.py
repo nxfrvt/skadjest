50eb95b834e1fecb8196a681ffb065f70d2814ea
Bartłomiej Mazurek <mazurekb02@gmail.com>
2020-12-20 21:31:21 +0100

    prep b4 updating weights; calc of sig, derivs and deltas

--- neural_network.py
+++ neural_network.py
@@ -15,7 +15,9 @@ charset = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A",
            "N", "O", "P", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
 
 training_data = []  # tablica zawierajaca dane trenujace z plików graficznych(fontu)
-training_labels = [[0 for _ in range(len(charset))] for _ in range(len(charset))]
+training_labels = np.array([[0 for _ in range(len(charset))] for _ in range(len(charset))])
+
+
 # ^tablica "labelujaca" dane z training_data. Przy deklaracji wypelniania zerami do dimension = 35x35
 
 
@@ -23,7 +25,7 @@ def sigmoid(array: np.array) -> np.array:
     """
     1/(1+exp(-S))
     """
-    aux_list = list
+    aux_list = []
     for x in array:
         result = 1 / (1 + np.exp(-x))
         aux_list.append(result)
@@ -35,7 +37,7 @@ def sigmoid_derivative(sig: np.array) -> np.array:
     """
     f'(x) = x*(1-x)
     """
-    aux_list = list
+    aux_list = []
     for x in sig:
         result = x * (1 - x)
         aux_list.append(result)
@@ -53,6 +55,7 @@ def _load_training_data():
     for char in charset:
         image = cv2.imread('rsc/training_data/' + str(char) + '.png')
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image = image.reshape(800, 1)
         training_data.append(image / 255)
         training_labels[i][i] = 1
         i += 1
@@ -66,16 +69,37 @@ class NeuralNetwork:
                        [np.ndarray, np.ndarray],  # warstwa ukryta [wagi], [bias]
                        [np.ndarray, np.ndarray]]  # warstwa wyjsciowa [wagi], [bias]
         self.__init_layers()
+        self.activations = [0, 0, 0]
+        self.derivatives = [0, 0, 0]
+        self.delta = [0, 0, 0]
 
     def __init_layers(self):
         """Initializes all 3 layers with random values (weights and biases)"""
         for layer in range(len(self.layers)):
             self.layers[layer][0] = np.random.rand(self.neurons[layer + 1], self.neurons[layer])  # 800 - wymiary obrazu
-            self.layers[layer][1] = np.random.rand(self.neurons[layer + 1])
+            self.layers[layer][1] = np.random.rand(self.neurons[layer + 1], 1)
 
     def train(self):
         _load_training_data()
-        # TODO input_data input_label
+        for i in range(len(training_data)):
+            self.activations[0] = sigmoid(
+                self.layers[0][0] @ training_data[i] + self.layers[0][1])  # warstwa poczatkowa
+            for layer in range(1, len(self.layers)):  # propagacja wprzod
+                self.activations[layer] = \
+                    sigmoid(self.layers[layer][0] @ self.activations[layer - 1] + self.layers[layer][1])
+
+            for layer in range(len(self.layers)):  # propagacja wstecz
+                self.derivatives[layer] = sigmoid_derivative(self.activations[layer])
+
+            label = np.array(training_labels[i].reshape(self.neurons[-1], 1))
+            self.delta[len(self.layers) - 1] = (label - self.activations[len(self.layers) - 1]) \
+                                               * self.derivatives[len(self.layers) - 1]
+            for layer in range(len(self.layers) - 2, 0, -1):
+                self.delta[layer] = (
+                            self.layers[layer + 1][0].transpose() @ self.delta[layer + 1] * self.derivatives[layer])
+
+
+        # TODO trenowanie
         pass
 
 
