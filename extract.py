7af705d17460448e7effd96987eb987a6f849482
nxfrvt <mazurekb02@gmail.com>
2020-12-28 18:37:34 +0100

    removal of inside rectangles and cropping out chars

--- extract.py
+++ extract.py
@@ -53,14 +53,33 @@ def extract_chars(plate):
     contours = sorted(contours, key=cv2.contourArea, reverse=True)[:15]
     # cv2.drawContours(plate, contours, -1, (0, 255, 0), 2)
 
-    #### create one bounding box for every contour found
+    #  create one bounding box for every contour found
     bb_list = []
     for c in contours:
         bb = cv2.boundingRect(c)
         bb_list.append(bb)
 
     # removing rectangles that overlap existing ones
-    threshold = 5  # próg kiedy rectangle zostaje odrzucony jako taki sam
+    bb_list = remove_overlaps(bb_list)
+    # removing rectangles that are inside of other rectangles
+    bb_list = remove_insides(bb_list)
+
+    # debug: draw boxes
+    img_boxes = plate.copy()
+    label = 1
+    for bb in bb_list:
+        x, y, w, h = bb
+        cv2.rectangle(img_boxes, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        img_cropped = plate[y:y+h, x:x+w]
+        cv2.imwrite('tmp/'+ str(label) + '.jpg', img_cropped)
+        # cv2.imshow(str(labul), img_boxes)
+        label += 1
+
+    return img_boxes
+
+
+def remove_overlaps(bb_list):
+    threshold = 5  # próg kiedy rectangle zostaje odrzucony jako taki sam, 5 wydaje sie git
     index = 0
     while index < len(bb_list):
         rect = bb_list[index]
@@ -71,21 +90,26 @@ def extract_chars(plate):
                 bb_list.remove(other_rect)
                 index -= 1
         index += 1
-    # removing rectangles that are inside of other rectangles
-    # debug: draw boxes
-    img_boxes = plate.copy()
-    # labul = 1
-    for bb in bb_list:
-        x, y, w, h = bb
-        cv2.rectangle(img_boxes, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        # cv2.imshow(str(labul), img_boxes)
-        # cv2.waitKey(0)
-        # labul += 1
+    return bb_list
 
-    return img_boxes
+
+def remove_insides(bb_list):
+    index = 0
+    while index < len(bb_list):
+        x1, y1, w1, h1 = bb_list[index]
+        for other_rect in bb_list[index + 1:]:
+            x2, y2, w2, h2 = other_rect
+            if ((x1 < x2) and
+                    (y1 < y2) and
+                    (x1 + w1 > x2 + w2) and
+                    (y1 + h1 > y2 + h2)):
+                bb_list.remove(other_rect)
+                index -= 1
+        index += 1
+    return bb_list
 
 
 if __name__ == '__main__':
-    x = find_license_plate('rsc/photos/tab_004.jpg')
+    x = find_license_plate('rsc/photos/tab_001.jpg')
     cv2.imshow('car', extract_chars(x))
     cv2.waitKey(0)
